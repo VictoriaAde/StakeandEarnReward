ebf9b6289bf53fd078e1e11fc4af8d8bd0ca5c24
Victoria <adedayovicky123@gmail.com>
2024-02-17 16:19:33 +0100

    TEST: should revert with error if not time for withdrawal

--- test/Stake.ts
+++ test/Stake.ts
@@ -49,18 +49,51 @@ describe("Stake", function () {
     });
   });
 
-  // describe("Reward", function () {
-  //   it("Should pass if reward is accurately 10% every minute", async function () {
-  //     const { stake } = await loadFixture(deployOneYearLockFixture);
-  //   });
-  // });
-
   describe("Reward", function () {
+    // it("Should calculate and add reward to the user's balance successfully", async function () {
+    //   const { stake, owner } = await loadFixture(deployOneYearLockFixture);
+
+    //   // Deposit some Ether to earn rewards
+    //   await stake.deposit({ value: ethers.parseEther("1") });
+
+    //   // Advance time by  1 minute to earn rewards
+    //   await ethers.provider.send("evm_increaseTime", [60]);
+    //   await ethers.provider.send("evm_mine");
+
+    //   // Check the reward
+    //   const rewardBeforeWithdrawal = await stake.checkReward();
+    //   expect(rewardBeforeWithdrawal).to.be.gt(0);
+
+    //   // Withdraw the staked Ether and the reward
+    //   await stake.withdraw(ethers.parseEther("1"));
+
+    //   // The user's balance should now be less than the initial stake due to the reward being subtracted
+    //   const stakerBalAfterWithdrawal = await stake.stakers(owner.address);
+    //   expect(stakerBalAfterWithdrawal).to.be.lt(ethers.parseEther("1"));
+
+    //   // The reward should be greater than  0 and less than or equal to the initial stake
+    //   expect(rewardBeforeWithdrawal).to.be.lt(ethers.parseEther("1"));
+    //   expect(rewardBeforeWithdrawal).to.be.gt(0);
+    // });
+
     it("Should revert with error when users are trying to checkReward without staking", async function () {
       const { stake } = await loadFixture(deployOneYearLockFixture);
 
       // Attempt to check reward without staking
       expect(stake.checkReward()).to.be.revertedWithCustomError;
     });
+  });
+
+  describe("Withdrawal", function () {
+    it("Should revert with not_time_for_withdrawal error if called too soon", async function () {
+      const { stake } = await loadFixture(deployOneYearLockFixture);
+      await stake.deposit({ value: ethers.parseEther("1") });
+      expect(stake.withdraw(1000000000000000000n)).to.be
+        .revertedWithCustomError;
+    });
+
+    // it("Should pass if withdraw was successful", async function () {
+    //   const { stake } = await loadFixture(deployOneYearLockFixture);
+    // });
   });
 });
